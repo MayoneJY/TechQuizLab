525d01c00ff7fcb476ce3002b7d6e7db8c9ab2d3
SOO <chzhvnemf009@gmail.com>
2025-12-09 14:25:21 +0900

    feat: 게시글 좋아요 확인 및 목록 조회 기능 백엔드 구현

---
+++ server/src/main/java/com/mayonedev/battle/domain/board/service/BoardPostlikeService.java
@@ -0,0 +1,30 @@
+﻿package com.mayonedev.battle.domain.board.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.mayonedev.battle.domain.board.dto.BoardPostlikeDto;
+import com.mayonedev.battle.domain.board.entity.PostLike;
+
+public interface BoardPostlikeService {
+
+	// 좋아요 추가
+	void insertPostLike(PostLike postLike) throws Exception;
+
+	// 좋아요 삭제
+	void deletePostLike(BoardPostlikeDto postlikeParm) throws Exception;
+
+	// 게시글의 좋아요 개수 조회
+	int countPostLike(BoardPostlikeDto postlikeParm) throws Exception;
+
+	// 사용자가 특정 게시글에 좋아요를 눌렀는지 확인
+	boolean checkPostLike(BoardPostlikeDto postlikeParm) throws Exception;
+
+	// 게시글의 좋아요 목록 조회
+	List<PostLike> selectPostLikeByPost(BoardPostlikeDto postlikeParm) throws Exception;
+
+	// 사용자가 좋아요한 게시글 목록 조회
+	List<PostLike> selectPostLikeByUser(long user_id) throws Exception;
+
+}
+
